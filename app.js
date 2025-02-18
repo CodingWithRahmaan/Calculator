2d8801766dbb67413d4444792f6b3a57064dfef2
CodingWithRahmaan <abdurrehmanbasra1@gmail.com>
2025-02-18 19:30:58 +0400

    ..

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 function btnClick(val) {
-    let cal = document.getElementById("calculation");
+   let cal = document.getElementById("calculation")
+  
+  let lastChar = cal.value.slice(-1);
 
-    // let lsatChar = cal.value[cal.value.slice (- 1)];
-    
-    if (val === "C") {
-        cal.value = "";
-       
-     }
-    else if (val === "CE") {
-        cal.value = cal.value.slice(0, -1);
-    }
+  if( val === "C"){
+    cal.value = "" 
+  }
+  else if((lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/") && (val === "+"|| val === "-" || val === "*" || val === "/")){
+    cal.value = cal.value.slice(0,-1) + val
+  } else if(val  === "CE"){
+    cal.value = cal.value.slice(0,-1)
 
-      else if (val === "=") {
-        cal.value = eval(cal.value);
-    }
-    else{
-        cal.value += val;
-    }
+  }else if(val=== "="){
+    cal.value = eval(cal.value)
+  }else{
+    cal.value = cal.value + val
+  }
+
 }
+
+
+
+
 
